125a4a48a35fcf1cd8074f2cabc61908cacb3834
Tommy Hoang <tommyyhoangg@gmail.com>
2023-01-12 16:14:51 +0100

    Added menu for mobile

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './navbar.module.scss';
 import { Link } from 'react-router-dom';
+import Menu from './components/Menu';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={css['nav']}>
       <Link to={'/'}>
         <h2>Tommy Hoang</h2>
       </Link>
 
-      <h4>menu</h4>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+      <h4 onClick={() => setMenuOpen(true)}>menu</h4>
     </nav>
   );
 };
---
+++ client/src/components/Navbar/components/Menu.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import css from './menu.module.scss';
+import { Link } from 'react-router-dom';
+
+const Menu = ({ menuOpen, setMenuOpen }) => {
+  return (
+    <div
+      className={menuOpen ? `${css['menu']} ${css['menu-open']}` : css['menu']}
+    >
+      <div className={css['menu-top']}>
+        <Link to={'/'}>
+          <h2>Tommy Hoang</h2>
+        </Link>
+        <h4 onClick={() => setMenuOpen(false)}>close</h4>
+      </div>
+
+      <div
+        className={
+          menuOpen
+            ? `${css['menu-content']} ${css['menu-content-visible']}`
+            : css['menu-content']
+        }
+      >
+        <div className={css['menu-links']}>
+          <ul>
+            <li>
+              <Link to={'/'}>my work</Link>
+            </li>
+            <li>
+              <Link to={'/'}>socials</Link>
+            </li>
+            <li>
+              <Link to={'/'}>
+                <u>contact me</u>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className={css['menu-info']}>
+          <ul>
+            <li>Web Developer / Designer</li>
+            <li className={css['menu-info-1-half']}>Full Time Freelancer</li>
+            <li>Based in Pilsen</li>
+            <li>Czech Republic</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
